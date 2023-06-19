7ed05714fe4803ff81ecf4ac12fe308221f039fc
Jorge Oliart <jorgeoliart@users.noreply.github.com>
2023-06-19 17:45:09 -0500

    Convertir las vaiables a correlacionar en data tipo numeric

--- andreaespejo_cuanti2.R
+++ andreaespejo_cuanti2.R
@@ -75,3 +75,6 @@ tabla_chicuadrado <- chisq.test(muestra_n2500$IS3G16B, muestra_n2500$IS3G15J)
 
 #PREGUNTA 5
 
+#convertir las variables a ser correlacionadas a data tipo "numeric"
+muestra_n2500$PV1CIV <- as.numeric(muestra_n2500$PV1CIV)
+muestra_n2500$S_POLPART <- as.numeric(muestra_n2500$S_POLPART)
