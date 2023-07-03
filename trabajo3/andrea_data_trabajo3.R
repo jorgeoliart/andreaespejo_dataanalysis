36cc1be9450ac35135b5c63a02cbb15a2370b22f
Jorge Oliart <jorgeoliart@users.noreply.github.com>
2023-07-03 14:02:08 -0500

    Importar base de datos original.dta

--- trabajo3/andrea_data_trabajo3.R
+++ trabajo3/andrea_data_trabajo3.R
@@ -6,3 +6,9 @@ R.Version()
 
 #cargar las librerías a ser usadas
 library(haven) #cargar la librería que permite importar bases de datos en formato .dta
+
+library(dplyr) #cargar librería: dplyr, Versión: 1.1.2, que permite agrupar y resumir estadísticos
+
+#importar base de datos original.dta
+base_original <- read_dta('original.dta')
+
