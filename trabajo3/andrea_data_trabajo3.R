77f065457c726be13ffd8b1be045849e8f053ae4
Jorge Oliart <jorgeoliart@users.noreply.github.com>
2023-07-03 13:50:29 -0500

    Identificar la versión de R que se está usando

---
+++ trabajo3/andrea_data_trabajo3.R
@@ -0,0 +1,5 @@
+set.seed(280851373) #fijar set.seed para fijar la aleatorización de la muestra
+
+#asegurarse de usar las versiones de R y de las librerías instaladas para poder reproducir el código
+R.Version()
+#versión de R: 4.3.0
